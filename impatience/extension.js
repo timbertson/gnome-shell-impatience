95ba80239346d3ccd402a5ef8e64b73c7e6c4cad
gfxmonk <tim@gfxmonk.net>
2012-04-10 15:17:58 +1000

    detect whether schema is installed before trying to new() it

--- impatience/extension.js
+++ impatience/extension.js
@@ -1,39 +1,34 @@
 const St = imports.gi.St;
 const Gio = imports.gi.Gio;
 const Lang = imports.lang;
+const DEFAULT_SPEED = 0.75;
 
 var get_local_gsettings = function() {
 	LOG("initting schemas");
-	const GioSSS = Gio.SettingsSchemaSource;
 	const SCHEMA_PATH = 'org.gnome.shell.extensions.net.gfxmonk.impatience';
 
-	var schemaSource;
-	try {
-		const Extension = imports.misc.extensionUtils.getCurrentExtension();
-		let schemaDir = Extension.dir.get_child('schemas');
-		schemaSource = GioSSS.new_from_directory(
-			schemaDir.get_path(),
-			GioSSS.get_default(),
-			false);
-		let schemaObj = schemaSource.lookup(SCHEMA_PATH, true);
-		if (!schemaObj) {
-			throw new Error(
-				'Schema ' + schema +
-				' could not be found for extension ' +
-				Extension.metadata.uuid
-			);
-		}
-		return new Gio.Settings({ settings_schema: schemaObj });
-	} catch(e) {
-		if(imports.gi.GLib.getenv('GS_IMPATIENCE_SCHEMAS_INSTALLED') == 'true') {
-			// HACK: call this only when we think it'll work,
-			// otherwise the whole shell dies with no chance of recovery :(
-			LOG("Couldn't get local schema: " + e);
-			return new Gio.Settings({schema: SCHEMA_PATH});
-		}
-		throw e;
+	if(Gio.Settings.list_schemas().indexOf(SCHEMA_PATH) != -1) {
+		LOG("Using sytem-installed schema.");
+		return new Gio.Settings({schema: SCHEMA_PATH});
 	}
 
+	LOG("Attempting load of extension-local schema.");
+	const GioSSS = Gio.SettingsSchemaSource;
+	const Extension = imports.misc.extensionUtils.getCurrentExtension();
+	let schemaDir = Extension.dir.get_child('schemas');
+	let schemaSource = GioSSS.new_from_directory(
+		schemaDir.get_path(),
+		GioSSS.get_default(),
+		false);
+	let schemaObj = schemaSource.lookup(SCHEMA_PATH, true);
+	if (!schemaObj) {
+		throw new Error(
+			'Schema ' + schema +
+			' could not be found for extension ' +
+			Extension.metadata.uuid
+		);
+	}
+	return new Gio.Settings({ settings_schema: schemaObj });
 };
 
 function LOG(m){
@@ -49,7 +44,7 @@ Ext.prototype = {};
 Ext.prototype._init = function() {
 	this.enabled = false;
 	this.original_speed = St.get_slow_down_factor();
-	this.modified_speed = 0.75;
+	this.modified_speed = DEFAULT_SPEED;
 	this.unbind = noop;
 };
 
@@ -83,7 +78,7 @@ Ext.prototype.disable = function() {
 
 Ext.prototype.set_speed = function(new_speed) {
 	if(!this.enabled) {
-		LOG("NOT setting new speed (since the extension is disabled. how did this even happen?)");
+		LOG("NOT setting new speed, since the extension is disabled.");
 		return;
 	}
 	if(new_speed !== undefined) {
